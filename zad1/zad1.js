3706600f8f5f653bda73b3c1fbc0eb4709119580
Gitros <31899790+Gitros@users.noreply.github.com>
2022-07-16 16:33:48 +0200

    z1

---
+++ zad1/zad1.js
@@ -0,0 +1,8 @@
+const resultOut = document.getElementById('result-out');
+const nameInput = document.getElementById('name');
+const surnameInput = document.getElementById('surname');
+const dateInput = document.getElementById('date');
+const resultBtn = document.getElementById('result');
+
+result.textcontent = (nameInput.value + surnameInput.value + dateInput.value);
+resultBtn.addEventListener('click', result)
